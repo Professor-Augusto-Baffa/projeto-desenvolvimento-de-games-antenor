cf7473946ad95284b85f90f70ff569c8c719ca01
antenor-z <antenor-z@pm.me>
2024-05-20 10:25:31 -0300

    show arrows along the path to help the player

--- AirPath.cs
+++ AirPath.cs
@@ -3,19 +3,45 @@ using System;
 
 public partial class AirPath : Path2D
 {
+	[Export]
+    public Texture2D ArrowTexture;
+	[Export]
+    public int ArrowInterval = 20;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Line2D l = new (){
-		// 	DefaultColor = new Color(0,0,0,0.2f),
-		// 	Width = 2,
-		// };
-	
-		// foreach (var point in Curve.GetBakedPoints())
-		// {
-		// 	l.AddPoint(point + Position);
-		// }
-		// GetNode<Node2D>("/root/RootScene/GameScene/").CallDeferred("add_child", l);
+		if (ArrowTexture == null)
+        {
+            GD.PrintErr("ArrowTexture not assigned.");
+            return;
+        }
+
+        var points = Curve.GetBakedPoints();
+
+		var length = Curve.GetBakedLength();
+        for (int i = 0; i < points.Length; i += ArrowInterval)
+        {
+            Sprite2D arrow = new Sprite2D
+            {
+                Texture = ArrowTexture,
+                Position = points[i] + Position,
+            };
+
+            // Calculate the rotation angle for the arrow sprite
+            Vector2 direction;
+            if (i < points.Length - 1)
+            {
+                direction = (points[i + 1] - points[i]).Normalized();
+            }
+            else
+            {
+                direction = (points[i] - points[i - 1]).Normalized();
+            }
+            float angle = direction.Angle();
+            arrow.Rotation = angle + (float)Math.PI / 2;
+
+            GetNode<Node2D>("/root/RootScene/GameScene/").CallDeferred("add_child", arrow);
+        }
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
