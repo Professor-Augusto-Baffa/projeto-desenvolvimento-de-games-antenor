7a0bb7075e94b7b9bb017f4abf663db8070c510f
antenor-z <antenor-z@pm.me>
2024-06-09 11:24:42 -0300

    .

--- AirGateNarrow.cs
+++ AirGateNarrow.cs
@@ -11,15 +11,6 @@ public partial class AirGateNarrow : Node2D
 	 */
 	private bool hasExplodedJustBefore = false;
 	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
 private void _on_front_area_entered(Area2D area)
 	{
